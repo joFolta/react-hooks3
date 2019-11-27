4c36d0ec5d4fc37d8a8d3dcf8a2032ddfd5755a3
Johann Folta <jfolta@kyruus.com>
2019-11-26 22:50:20 -0500

    rename App to Counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-function App() {
+function Counter() {
   const [count, setCount] = useState(0);
   const increment = () => setCount(count + 1);
   return <button onClick={increment}>{count}</button>;
 }
 
-export default App;
+export default Counter;
