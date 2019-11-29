39ff00918bd512871b6faee404f0e56446285ac2
Johann Folta <jfolta@kyruus.com>
2019-11-29 14:30:18 -0500

    need to reset between tests

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -9,3 +9,21 @@ test("counter increments the count", () => {
   fireEvent.click(button);
   expect(button.textContent).toBe("1");
 });
+
+test("reads and writes to localStorage", () => {
+  window.localStorage.setItem("count", "3");
+  const { container } = render(<Counter />);
+  const button = container.firstChild;
+  expect(button.textContent).toBe("3");
+  fireEvent.click(button);
+  expect(window.localStorage.getItem("count")).toBe("4");
+  expect(button.textContent).toBe("4");
+  // window.localStorage.removeItem("count");   //clean up localStorage 'count' for future tests
+});
+
+// this test fails b/c the reset above is commented out (button.textContent is still '4')
+test("another test...", () => {
+  const { container } = render(<Counter />);
+  const button = container.firstChild;
+  expect(button.textContent).toBe("0");
+});
