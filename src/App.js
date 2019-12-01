f7c32b88af59bd5dcf0926e8d614d2597528b7f6
Johann Folta <jfolta@kyruus.com>
2019-12-01 18:25:10 -0500

    boiler plate code setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React, { useState } from "react";
+import fetchPokemon from "./fetch-pokemon";
+
+const cache = {};
+
+function PokemonInfo({ pokemonName }) {
+  const pokemon = cache[pokemonName];
+  return <pre>{JSON.stringify(pokemon || "Unknown", null, "___")}</pre>;
+}
 
 function App() {
-  return <h1>Blank Template</h1>;
+  const [pokemonName, setPokemonName] = useState(null);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setPokemonName(e.target.elements.pokemonName.value);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="pokemonName-input">Pokemon Name (ie. Pikachu)</label>
+        <input id="pokemonName-input" name="pokemonName" />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {pokemonName ? <PokemonInfo pokemonName={pokemonName} /> : null}
+      </div>
+    </div>
+  );
 }
 
 export default App;
