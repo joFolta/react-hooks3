0775f995724cfd2fcd24723045bbd999b715eeea
Johann Folta <jfolta@kyruus.com>
2019-12-01 13:09:20 -0500

    complete vid11

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-function App() {
-  return <h1>Blank Template</h1>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import createActivityDetector from "activity-detector";
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// useIdle - Custom Hook
+function useIdle(options) {
+  const [isIdle, setIsIdle] = React.useState(false);
+  React.useEffect(() => {
+    const activityDetector = createActivityDetector(options);
+    activityDetector.on("idle", () => setIsIdle(true));
+    activityDetector.on("active", () => setIsIdle(false));
+    return () => activityDetector.stop(); // cleanup function (to avoid memory leaks)
+  }, []); // empty array, so it only runs onMount of component, rather than every re-render
+  return isIdle;
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App() {
+  const isIdle = useIdle({ timeToIdle: 1000 }); // options: idle after 1 sec
+  return <div>{isIdle ? "Are you still there?" : "Hello there!"}</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
